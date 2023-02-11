14c489106bacd01d79ab134224a7f42d2308de35
hwelsters <corevalue.noel@gmail.com>
2023-02-10 20:06:22 -0700

    Prisma Schema

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 /*
+
      _                 _      _
     | |_ __ __ __ ___ | | ___| |_  ___  _ _  ___
     | ' \\ V  V // -_)| |(_-<|  _|/ -_)| '_|(_-<
     |_||_|\_/\_/ \___||_|/__/ \__|\___||_|  /__/
                 Sup dudes!
+
 */
 
 import express, { type Request, type Response } from "express";
