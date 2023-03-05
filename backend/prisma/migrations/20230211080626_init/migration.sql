e33734555eff3c71156454eaa27e99fd7719d753
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-03-05 12:14:11 -0700

    Setup obnoxious typing

--- backend/prisma/migrations/20230211080626_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Hwelster" (
-    "id" INTEGER NOT NULL,
-    "email" TEXT NOT NULL,
-
-    CONSTRAINT "Hwelster_pkey" PRIMARY KEY ("id")
-);
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,21 +8,28 @@
 
 */
 
-import express, { type Request, type Response } from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
+import express, { type Application, type Request, type Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
 
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import validateEnv from "./utils/validate_env";
 
-import validateEnv from './utils/validate_env'
-dotenv.config()
-validateEnv()
+dotenv.config();
+validateEnv();
 
-const app = express()
-app.use(cors())
-app.use(morgan('tiny'))
+const app : Application = express();
+app.use(cors());
+app.use(morgan("tiny"));
 
-const onHealthCheck = (_: Request, response: Response): Response<any, Record<string, any>> => response.status(200).json('HEALTH CHECK :: Health checked! 💉')
-app.get('/api', onHealthCheck)
+const onHealthCheck = (_: Request, response: Response): Response<any, Record<string, any>> => response.status(200).json("HEALTH CHECK :: Health checked! 💉");
+app.get("/api", onHealthCheck);
 
-export default app
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+app.get("/db-test", async () => {
+  const allUsers = await prisma.hwelster.findMany();
+  console.log(allUsers);
+});
+
+export default app;
