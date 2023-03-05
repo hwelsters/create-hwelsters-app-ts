64f6af578ad678551f763252d839b01f100b1571
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-03-05 11:38:14 -0700

    Docker Compose Development Hot Reloading

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,13 @@
+/*
+
+     _                 _      _
+    | |_ __ __ __ ___ | | ___| |_  ___  _ _  ___
+    | ' \\ V  V // -_)| |(_-<|  _|/ -_)| '_|(_-<
+    |_||_|\_/\_/ \___||_|/__/ \__|\___||_|  /__/
+                Sup dudes!
+
+*/
+
 import express, { type Request, type Response } from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
