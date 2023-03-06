e4cdcc91162d3472b2802e7e48409b91dad9fc20
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-03-05 18:57:04 -0700

    Deployed terraform from AWS' us-west-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,11 +10,13 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  # region = "us-east-2"
+  region = "us-west-1"
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
+  ami                    = "ami-0d221cb540e0015f4"
+  # ami                    = "ami-060d3509162bcc386"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
