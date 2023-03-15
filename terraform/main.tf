832f4641b0d2732b45acd91e9919aff09707cf02
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-03-14 21:23:40 -0700

    Integration tests with Github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,32 +23,62 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0d221cb540e0015f4"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
-  user_data_replace_on_change = true
+resource "aws_s3_bucket" "bucky" {
+  bucket = "loveneet-bucket"
 
   tags = {
-    Name = "${var.name}-instance"
+    Name        = "My bucket"
   }
 }
 
-resource "aws_security_group" "instance" {
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.bucky.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
 
-  name = "${var.name}-security-group"
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
 
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.bucky.arn}/*",
+    ]
   }
 }
+
+# resource "aws_instance" "bucky" {
+#   ami                    = "ami-0d221cb540e0015f4"
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.instance.id]
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World" > index.html
+#               nohup busybox httpd -f -p ${var.server_port} &
+#               EOF
+
+#   user_data_replace_on_change = true
+
+#   tags = {
+#     Name = "${var.name}-instance"
+#   }
+# }
+
+# resource "aws_security_group" "instance" {
+
+#   name = "${var.name}-security-group"
+
+#   ingress {
+#     from_port   = var.server_port
+#     to_port     = var.server_port
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
