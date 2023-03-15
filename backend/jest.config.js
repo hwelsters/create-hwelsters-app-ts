e7ecb1df7b49fef1a70257a6689014102fce43df
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-03-15 14:53:55 -0700

    I prefer __tests__ more

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src'],
-  setupFiles: ["<rootDir>/tests/config/set_env.ts"],
+  setupFiles: ["<rootDir>/__tests__/config/set_env.ts"],
   testMatch: ['**/?(*.)+(test).ts']
 }
